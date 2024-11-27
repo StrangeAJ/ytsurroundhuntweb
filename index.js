40bfc2c3046e1109884b9de377961c673737bff9
Ashish Jingar <ashish.jingar@oracle.com>
2024-11-27 20:31:41 +0530

    used Invidious Instead

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const express = require('express');
 const path = require('path');
-const ytdl = require("@distube/ytdl-core");
-const yts = require('yt-search');
-// cors
+const axios = require('axios');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 10000;
+const INVIDIOUS_URL = process.env.INVIDIOUS_URL;
 
+const surround_formats = ['256', '258', '325', '327', '328', '338', '380'];
+let vids = [];
 
-async function getSurroundingAudios(vidIds) {
+async function searchYouTube(query) {
+    let availableFormats = [];
     let vidIdswithSurroundSound = [];
-    let availableFormats = {};
-
-    for (const vidId of vidIds) {
-        try {
-            const info = await ytdl.getInfo(vidId);
-            const formats = info.player_response.streamingData.adaptiveFormats.filter(format => format.audioChannels === 6);
-            if(formats.length > 0) {
-                vidIdswithSurroundSound.push(vidId);
-                const itags = formats.map(format => format.itag);
-                availableFormats[vidId] = itags;
+    const url = `${INVIDIOUS_URL}/api/v1/search?type=video&q=${query}`;
+    try {
+        const response = await axios.get(url);
+        vids = response.data;
+        for (const video of vids) {
+            const videoId = video.videoId;
+            const videoUrl = `${INVIDIOUS_URL}/api/v1/videos/${videoId}`;
+            const videoResponse = await axios.get(videoUrl, {
+                headers: {
+                    'Host': INVIDIOUS_URL.split('//')[1],
+                    'Cookie': 'INVIDIOUS_SERVER_ID=2'
+                }
+            });
+            const videoData = videoResponse.data;
+            const formats = videoData.adaptiveFormats;
+            const availableSurroundFormats = formats.filter((format) => {
+                return surround_formats.includes(format.itag);
+            });
+            if (availableSurroundFormats.length > 0) {
+                vidIdswithSurroundSound.push(videoId);
+                const itags = availableSurroundFormats.map(format => format.itag);
+                availableFormats.push({
+                    id: videoId,
+                    formats : itags,
+                    title: videoData.title,
+                    url: 'https://inv.nadeko.net/watch?v=' + videoId
+                });
             }
-        } catch (err) {
-            console.log(err);
         }
+    } catch (err) {
+        console.error(err);
     }
-
-    return { vidIdswithSurroundSound, availableFormats };
+    return { availableFormats };
 }
 
 app.set('view engine', 'ejs');
@@ -45,28 +64,12 @@ app.post('/', async (req, res) => {
     }
 
     try {
-        const results = await yts({
-            query: req.body.query,
-            pages: 2,
-            search: "video",
-        });
-        const vids = results.videos;
-        const vidIds = vids.map(vid => vid.videoId);
-        const { vidIdswithSurroundSound, availableFormats } = await getSurroundingAudios(vidIds);
-        const length = vids.length;
-        console.log(length);
-        const formattedResults = vidIdswithSurroundSound.map(vidId => {
-            const video = vids.find(vid => vid.videoId === vidId);
-            return {
-                title: video.title,
-                url: video.url,
-                formats: availableFormats[vidId]
-            };
-        });
-
-        res.render('index', { results: formattedResults });
+        const { availableFormats } = await searchYouTube(req.body.query);
+        // const responseForUI to be set according to availableFormats
+        console.log(availableFormats);
+        res.render('index', { results: availableFormats });
     } catch (err) {
-        console.log(err);
+        console.error(err);
         res.send("An error occurred while processing your request.");
     }
 });
